8f99f64fc8e4c12020163228b30c755e665eb55e
nilank nikhil <nilanknikhil@gmail.com>
2025-06-19 21:39:30 +0530

    Tested

--- harbour_space/models.py
+++ harbour_space/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class StorageServer(models.Model):
+    address = models.CharField(max_length=255)
+
+
+class File(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Chunk(models.Model):
+    file = models.ForeignKey(File, on_delete=models.CASCADE, related_name="chunks")
+    index = models.IntegerField()
+    storage_servers = models.ManyToManyField(StorageServer)
+    checksum = models.CharField(max_length=64, blank=True)
